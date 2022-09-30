795190c05aa430f3853f4825668ad547492d0d2d
Jodyrobert Brent Garren <brentgarren@yahoo.com>
2022-09-29 21:42:12 -0500

    update

--- variable.py
+++ variable.py
@@ -1,3 +1,7 @@
 welcome_message = ['Welcome to my Trove, Subject', '***Eats Cheese***', '***shines ring*** Another one, Welcome', 'Hi there, Hello, Did you bring fresh cheese for Ratty?', 'You have chosen wisely.', 'Ohhh Shiny.', "Don/'t pester me peasent."]
 
 ignore_message = ['*Lord Ratty refuses to acknowledge you*', '*stares*', '*eats cheese*', '**You will address me properly!**', "Why do you waste my time subject."]
+
+reactrolechannel = 1008821437416611950
+
+ruleschannel = 1021276883322675270
